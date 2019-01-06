25f27ecf48a28e15b10ada6bbae28d976f15f206
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-06 17:44:53 +0100

    validation

---
+++ FormRegister/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormRegister.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register()
+        {
+            var model = new ViewModels.RegisterViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Register(ViewModels.RegisterViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                model.RegistrationError = true;
+                return View(model);
+            }
+            model.Saved = true;
+            return View(model);
+        }
+
+    }
+}
---
+++ FormRegister/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormRegister.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public bool RegistrationError { get; set; }
+        public bool Saved { get; set; }
+
+        [Required(ErrorMessage ="Förnamn är ju obligartiskt dumsnut")]
+        public string Forname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public string Loginname { get; set; }
+
+        [Range(1000,9999)]
+        public int Pincode { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
