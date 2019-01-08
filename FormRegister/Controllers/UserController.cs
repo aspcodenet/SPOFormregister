ce649565e25b5c888ddae81e53b50bcfc852ae22
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-08 15:06:34 +0100

    Added VallidtionAttribute

--- FormRegister/Controllers/UserController.cs
+++ FormRegister/Controllers/UserController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FormRegister.ViewModels;
 
 namespace FormRegister.Controllers
 {
+
+
     public class UserController : Controller
     {
         [HttpGet]
@@ -19,6 +23,9 @@ namespace FormRegister.Controllers
         [HttpPost]
         public ActionResult Register(ViewModels.RegisterViewModel model)
         {
+//            ValidatePersonNummer(model);
+
+
             if(!ModelState.IsValid)
             {
                 model.RegistrationError = true;
@@ -28,5 +35,26 @@ namespace FormRegister.Controllers
             return View(model);
         }
 
+        private void ValidatePersonNummer(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) return;
+            DateTime dt;
+            if (model.PersonNummer.Length == 10 || model.PersonNummer.Length == 11)
+            {
+                if(!DateTime.TryParseExact(model.PersonNummer.Substring(0,6), "yyMMdd", null,System.Globalization.DateTimeStyles.AssumeLocal,out dt))
+                {
+                    ModelState.AddModelError("Personnummer", "Felaktigt personnummer");
+                }
+            }
+            else
+            {
+                if (!DateTime.TryParseExact(model.PersonNummer.Substring(0, 8), "yyyyMMdd", null, System.Globalization.DateTimeStyles.AssumeLocal, out dt))
+                {
+                    ModelState.AddModelError("Personnummer", "Felaktigt personnummer");
+                }
+            }
+
+
+        }
     }
 }
--- FormRegister/ViewModels/RegisterViewModel.cs
+++ FormRegister/ViewModels/RegisterViewModel.cs
@@ -6,6 +6,35 @@ using System.Web;
 
 namespace FormRegister.ViewModels
 {
+
+    public class CheckPersonNummer : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string personnummer = Convert.ToString(value);
+            DateTime dt;
+            if (personnummer.Length == 10 || personnummer.Length == 11)
+            {
+                if (!DateTime.TryParseExact(personnummer.Substring(0, 6), "yyMMdd", null, System.Globalization.DateTimeStyles.AssumeLocal, out dt))
+                {
+                    return new ValidationResult("felaktigt personnummer");
+                }
+            }
+            else
+            {
+                if (!DateTime.TryParseExact(personnummer.Substring(0, 8), "yyyyMMdd", null, System.Globalization.DateTimeStyles.AssumeLocal, out dt))
+                {
+                    return new ValidationResult("felaktigt personnummer");
+                }
+            }
+            return ValidationResult.Success;
+
+
+        }
+
+    }
+
+
     public class RegisterViewModel
     {
         public bool RegistrationError { get; set; }
@@ -25,6 +54,7 @@ namespace FormRegister.ViewModels
         public DateTime MedlemsDatum { get; set; }
 
         [RegularExpression(@"^(\d{6}|\d{8})[-|(\s)]{0,1}\d{4}$",ErrorMessage ="Felaktigt personnummer")]
+        [CheckPersonNummer]
         public string PersonNummer { get; set; } 
  
         [Range(1000,9999)]
